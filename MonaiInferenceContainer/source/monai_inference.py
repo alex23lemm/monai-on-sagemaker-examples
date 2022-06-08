8b46d1e89d7665f01eb23935e8738c5f946233c4
Benedetto Carollo <bcarollo@amazon.com>
2022-06-08 13:02:28 +0000

    extending the dockler container to test locally

--- MonaiInferenceContainer/source/monai_inference.py
+++ MonaiInferenceContainer/source/monai_inference.py
@@ -4,6 +4,9 @@
 import json
 import logging
 import sys
+import argparse
+import ast
+import os
 
 # External Dependencies:
 import torch
@@ -49,3 +52,46 @@ def output_fn(predictions, content_type):
     #res = predictions.cpu().numpy().tolist()
     res = " return from dummy prediction"
     return json.dumps(res)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=2,
+        metavar="W",
+        help="number of data loading workers (default: 2)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=2,
+        metavar="E",
+        help="number of total epochs to run (default: 2)",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=4, metavar="BS", help="batch size (default: 4)"
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.001,
+        metavar="LR",
+        help="initial learning rate (default: 0.001)",
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.9, metavar="M", help="momentum (default: 0.9)"
+    )
+    parser.add_argument(
+        "--dist-backend", type=str, default="gloo", help="distributed backend (default: gloo)"
+    )
+
+    # The parameters below retrieve their default values from SageMaker environment variables, which are
+    # instantiated by the SageMaker containers framework.
+    # https://github.com/aws/sagemaker-containers#how-a-script-is-executed-inside-the-container
+    parser.add_argument("--hosts", type=str, default=ast.literal_eval(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
